97475bd305f15e46dc3b14db7a12a0a2c86a69db
Dag-da <sylvain.leflon@gmail.com>
2022-08-02 14:13:57 +0200

    boucle games

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 $title = "Accueil"; // title for current page
-include("partials/header.php");
-require_once("helpers/pdo.php")
+include("partials/header.php"); // include header
+include("helpers/functions.php"); // include functions
+require_once("helpers/pdo.php");
+
+// Requête pour récupérer mes jeux
+$sql = "SELECT * FROM `jeux`";
+// Prépare la requête
+$query = $pdo->prepare($sql);
+// Stocker le résultat dans une variable
+$query->execute();
+$games = $query->fetchAll();
+// debug_array($games);
+// $games = [];
 ?>
 <!-- main content -->
 <div class="pt-16 wrap__content">
@@ -24,18 +35,28 @@ require_once("helpers/pdo.php")
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <th>1</th>
-                    <td>Mario</td>
-                    <td>Aventure</td>
-                    <td>Switch</td>
-                    <td>3</td>
-                    <td>12</td>
-                    <td><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="magnifying-glass" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 icon"><path fill="currentColor" d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" class=""></path></svg></td>
-                </tr>
+                <?php
+                if (count($games) < 1)
+                { 
+                ?>
+                    <td>Pas de jeux disponibles actuellement.</td>
+                <?php 
+                } 
+                else
+                foreach ($games as $game) : 
+                ?>
+                    <tr>
+                        <th><?= $game["id"] ?></th>
+                        <td><?= $game["name"] ?></td>
+                        <td><?= $game["plateform"] ?></td>
+                        <td><?= $game["price"] ?></td>
+                        <td><?= $game["price"] ?></td>
+                        <td><?= $game["pegi"] ?></td>
+                        <td><a href="show.php"><svg focusable="false" data-prefix="fas" data-icon="magnifying-glass" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 icon"><path fill="currentColor" d="M500.3 443.7l-119.7-119.7c27.22-40.41 40.65-90.9 33.46-144.7C401.8 87.79 326.8 13.32 235.2 1.723C99.01-15.51-15.51 99.01 1.724 235.2c11.6 91.64 86.08 166.7 177.6 178.9c53.8 7.189 104.3-6.236 144.7-33.46l119.7 119.7c15.62 15.62 40.95 15.62 56.57 0C515.9 484.7 515.9 459.3 500.3 443.7zM79.1 208c0-70.58 57.42-128 128-128s128 57.42 128 128c0 70.58-57.42 128-128 128S79.1 278.6 79.1 208z" class=""></path></svg></a></td>
+                    </tr> 
+                <?php endforeach ?>
             </tbody>
         </table>
-        
     </div>
 </div>
 <!-- footer -->
